4681944df3fc21b856c796d468a372548083e85a
Silpanchin <silpanchin246@gmail.com>
2026-09-22 12:44:29 -0400

    Agrego panel de administración

--- Silpancheria_Gestion/Program.cs
+++ Silpancheria_Gestion/Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// --- 1. CONFIGURAR CORS ---
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTodo", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Registrar Supabase
 builder.Services.AddSingleton<SupabaseService>();
 
@@ -24,11 +35,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// --- 2. HABILITAR CORS Y ARCHIVOS ESTÁTICOS ---
+app.UseCors("PermitirTodo");
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
